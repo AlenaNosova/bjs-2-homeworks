256169a947a802da50cdc72ba05371758399a1f2
AlenaNosova <Teplykh91@gmail.com>
2022-07-29 23:44:01 +0700

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,33 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  let d = b^2-4*a*c;
+  
+  if (d < 0) {
+    arr = [];
+  } if (d == 0) {
+    arr  = -b/(2*a);
+  } else if (d > 0) {
+    arr = (-b + Math.sqrt(d) )/(2*a) && (-b - Math.sqrt(d) )/(2*a);
+  }
   return arr; // array
 }
+
+"use strict";
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
+  let S = amount - contribution; 
+  let today = new Date();
+  let n = ((date.getMonth() - today.getMonth()) + ((date.getFullYear() - today.getFullYear()) *12));
+  let P = percent/12/100;
+  let payment = S * (P + (P / (((1 + P)^n) - 1)));
+  
+  totalAmount = (payment * n).toFixed(2);
+  
+  console.log(+totalAmount)
 
-  return totalAmount;
+  return +totalAmount;
 }
