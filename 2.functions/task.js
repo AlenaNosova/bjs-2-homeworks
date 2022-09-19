4bdb990c9fe7233c02564063c5204c825e9bdabe
AlenaNosova <Teplykh91@gmail.com>
2022-09-19 21:20:21 +0700

    first commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,18 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = arr[0];
+  max = arr[0];
+  sum = 0;
+  avg = sum/arr.length;
 
-  // Ваш код
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] > arr[i--]) max = arr[i]; 
+    if(arr[i] < arr[i--]) min = arr[i]; 
+    sum += arr[i];
+  }
+
+  +avg.toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
@@ -11,21 +21,31 @@ function getArrayParams(arr) {
 function worker(arr) {
   let sum;
 
-  // Ваш код
+  for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = arrOfArr[0];
+  func(arr);
 
-  // Ваш кода
-  // for ...
+  for(let i = 0; i < arrOfArr.length; i++) {
+    if(arrOfArr[i] > arr[i]) max = sum;
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+
+  let numb;
+  
+  for(let = i; i < arr.length; i++) {
+    numb = max - min;
+    Math.abs(numb);
+  }
 }
