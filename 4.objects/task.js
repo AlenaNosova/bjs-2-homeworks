67f94128b5326553c243bcbfa45dbb743853ae53
AlenaNosova <Teplykh91@gmail.com>
2022-09-25 18:26:55 +0700

    first commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    let student1 = new Object();
+    let student2 = new Object();
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  let marks = [];
+  this.mark = marks;
+
+  if(this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  this.marks = marks;
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  let average;
+  for(let i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+  average = sum / this.marks.length;
+  return average;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete subject;
+  delete marks;
+  this.excluded = reason;
+}
